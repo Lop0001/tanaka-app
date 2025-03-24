929e19cab3c6606657f88f8e46ee42893102d925
Lop0001 <joselopezpacaya@gmail.com>
2025-03-24 18:20:35 -0500

    feat: add product details view

--- src/main/java/edu/cibertec/tanaka_app/controller/ProductController.java
+++ src/main/java/edu/cibertec/tanaka_app/controller/ProductController.java
@@ -1,10 +1,12 @@
 package edu.cibertec.tanaka_app.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.cibertec.tanaka_app.entities.Product;
@@ -24,4 +26,12 @@ public class ProductController {
 		return "productos";
 	}
 	
+	@GetMapping("/{id}")
+	public String findById(@PathVariable Long id, Model model) {
+		
+		Optional<Product> oProduct = productService.findById(id);
+		System.out.println("mi objeto::::::" + oProduct);
+		model.addAttribute("producto",oProduct.get());
+		return "detalle-producto";
+	}
 }
